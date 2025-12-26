49d18a4943bc6199e14b9823cb7b56e26ecab7b0
Mohcine0619 <akkadmohcine07@gmail.com>
2025-12-26 20:14:36 +0100

    Created Comment model, migration, and set up all Eloquent relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,5 +8,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Post extends Model
 {
     use HasFactory;
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+    public function comment(){
+        return $this->hasMany(Comment::class);
+    }
 
 }
